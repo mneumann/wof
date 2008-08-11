48b5b72b084738af1d278b9d561c81afb25e7490
Michael Neumann <mneumann@ntecs.de>
2008-08-11 17:34:44 +0200

    Extend the example to run as a web application.

--- examples/counter.rb
+++ examples/counter.rb
@@ -28,31 +28,63 @@ class Counter < Component
     r.anchor.action(:dec!).with("--")
     r.text " #{ @value } "
     r.anchor.action(:inc!).with("++")
+    r.text "<br/>"
   end
 end
 
 if __FILE__ == $0
-  require 'pp'
 
-  root = Root.new
-  pp root
+  require 'rubygems'
+  require 'rack'
 
-  state = State.new
-  state.substate('1').with {|s|
-    s.set('value', "2")
-    s.actions({'inc!' => []})
-  }
+  class Handler
+    def call(env)
+      request = Rack::Request.new(env)
 
-  root.load_state(state)
-  pp root
+      root = Root.new
+      state = State.new
 
-  root.invoke_actions(state)
-  pp root
+      #
+      # Extract state out of params
+      #
 
-  new_state = root.dump_state(State.new)
-  pp new_state
+      expanded = []
+      request.params.each do |k,v|
+        if k.start_with?("/")
+          # absolute path
+          expanded << [k, v] 
+        else
+          expanded << [request.path_info + "/." + k, v]
+        end
+      end
 
-  renderer = Renderer.new(new_state)
-  renderer.render(root)
-  puts renderer.to_s
+      expanded.each do |k,v|
+        path, attribute = k.split(".", 2)
+        paths = path.split("/")
+
+        paths.shift if paths.first.empty?
+
+        cur_state = state
+        paths.each do |piece|
+          cur_state = state.substate(piece)
+        end
+        if attribute.end_with?('!')
+          cur_state.actions({attribute => []})  # TODO
+        else
+          cur_state.set(attribute, v)
+        end
+      end
+
+      root.load_state(state)
+      root.invoke_actions(state)
+      next_state = root.dump_state(State.new)
+
+      renderer = Renderer.new(next_state)
+      renderer.render(root)
+
+      [200, {'Content-type' => 'text/html'}, renderer.to_s]
+    end
+  end
+
+  Rack::Handler::WEBrick.run(Handler.new, :Port => 8082)
 end
