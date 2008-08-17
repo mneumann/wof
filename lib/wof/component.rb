b35674507903f26e71d792e168df528cb5df7916
Michael Neumann <mneumann@ntecs.de>
2008-08-17 15:17:04 +0200

    Reduce complexity of meta-programming state-stuff.

--- lib/wof/component.rb
+++ lib/wof/component.rb
@@ -1,72 +1,45 @@
-module ComponentStateMixin
-  STATES_LOCAL = Hash.new
-  STATES = Hash.new
-  STATES_COMMITTED = false
+class Component
+  attr_accessor :component_name, :parent_component
 
-  def inherited(subclass)
-    STATES_LOCAL[subclass] ||= {}
-  end
-
-  def self.included(klass)
-    STATES_LOCAL[klass] ||= {}
-    case klass
-    when Class
-    when Module
-      def klass.included(klass) ComponentStateMixin.included(klass) end
-      klass.extend(ComponentStateMixin)
-    end
-  end
-
-  def state(name=nil, hash={}) 
+  def self.state(name=nil, hash=nil)
+    @state ||= {}
     if name
-      STATES_LOCAL[self] ||= {}
+      hash ||= {}
       hash[:name] ||= name.to_s  
-      hash[:ivar] ||= "@#{name}" 
       hash[:external] ||= name.to_s 
+
+      attr_accessor hash[:name]
+
+      setter = "#{name}="
+      getter = "#{name}"
+
       case hash[:type]
       when :integer
         hash[:default] = 0 if not hash.has_key?(:default)
-        hash[:get_state] ||= proc {|st| st.get_integer(hash[:external], hash[:default]) }
+        hash[:load_state] ||= proc {|obj, st|
+          obj.send(setter, st.get_integer(hash[:external], hash[:default]))
+        }
       else
-        hash[:get_state] ||= proc {|st| st.get(hash[:external], hash[:default]) }
+        hash[:load_state] ||= proc {|obj, st|
+          obj.send(setter, st.get(hash[:external], hash[:default]))
+        }
       end
-      hash[:set_state] ||= proc {|st, val| st.set(hash[:external], val) if val != hash[:default] }
+      hash[:dump_state] ||= proc {|obj, st|
+        val = obj.send(getter)
+        st.set(hash[:external], val) if val != hash[:default]
+      }
 
-      STATES_LOCAL[self].update(hash[:name] => hash)
-    elsif STATES_COMMITTED == false
-      commit_states!
+      @state.update(hash[:name] => hash)
     end
-    return STATES[self]
+    @state
   end
-
-  def commit_states!
-    #
-    # Update all states
-    #
-    # This doesn't need to be run if name=nil, hash.empty? and
-    # !STATE[self].empty?
-    #
-    STATES_LOCAL.each_key do |klass|
-      h = {}
-      klass.ancestors.reverse.each {|k| h.update(STATES_LOCAL[k] || {}) }
-      STATES[klass] = h
-    end
-  end
-end
-
-class Component
-  class << self; include ComponentStateMixin end
-
-  attr_accessor :component_name, :parent_component
 
   #
   # Load the state of the component and that of all subcomponents
   # from +state+.
   #
   def load_state(state)
-    self.class.state.each_value {|hash|
-      instance_variable_set(hash[:ivar], hash[:get_state].call(state))
-    }
+    self.class.state.each_value {|hash| hash[:load_state].call(self, state) }
     each_child {|c| c.load_state(state.substate(c.component_name)) }
   end
 
@@ -75,9 +48,7 @@ class Component
   # +state+.
   #
   def dump_state(state)
-    self.class.state.each_value {|hash|
-      hash[:set_state].call(state, instance_variable_get(hash[:ivar]))
-    }
+    self.class.state.each_value {|hash| hash[:dump_state].call(self, state) }
     each_child {|c| c.dump_state(state.substate(c.component_name))}
     state
   end 
