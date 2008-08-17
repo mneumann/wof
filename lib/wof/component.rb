53b5cfe6c5868562c6185d920d776ee01861c6cd
Michael Neumann <mneumann@ntecs.de>
2008-08-17 14:27:10 +0200

    Component state meta-programming magic.

--- lib/wof/component.rb
+++ lib/wof/component.rb
@@ -1,4 +1,62 @@
+module ComponentStateMixin
+  STATES_LOCAL = Hash.new
+  STATES = Hash.new
+  STATES_COMMITTED = false
+
+  def inherited(subclass)
+    STATES_LOCAL[subclass] ||= {}
+  end
+
+  def self.included(klass)
+    STATES_LOCAL[klass] ||= {}
+    case klass
+    when Class
+    when Module
+      def klass.included(klass) ComponentStateMixin.included(klass) end
+      klass.extend(ComponentStateMixin)
+    end
+  end
+
+  def state(name=nil, hash={}) 
+    if name
+      STATES_LOCAL[self] ||= {}
+      hash[:name] ||= name.to_s  
+      hash[:ivar] ||= "@#{name}" 
+      hash[:external] ||= name.to_s 
+      case hash[:type]
+      when :integer
+        hash[:default] = 0 if not hash.has_key?(:default)
+        hash[:get_state] ||= proc {|st| st.get_integer(hash[:external], hash[:default]) }
+      else
+        hash[:get_state] ||= proc {|st| st.get(hash[:external], hash[:default]) }
+      end
+      hash[:set_state] ||= proc {|st, val| st.set(hash[:external], val) if val != hash[:default] }
+
+      STATES_LOCAL[self].update(hash[:name] => hash)
+    elsif STATES_COMMITTED == false
+      commit_states!
+    end
+    return STATES[self]
+  end
+
+  def commit_states!
+    #
+    # Update all states
+    #
+    # This doesn't need to be run if name=nil, hash.empty? and
+    # !STATE[self].empty?
+    #
+    STATES_LOCAL.each_key do |klass|
+      h = {}
+      klass.ancestors.reverse.each {|k| h.update(STATES_LOCAL[k] || {}) }
+      STATES[klass] = h
+    end
+  end
+end
+
 class Component
+  class << self; include ComponentStateMixin end
+
   attr_accessor :component_name, :parent_component
 
   #
@@ -6,6 +64,9 @@ class Component
   # from +state+.
   #
   def load_state(state)
+    self.class.state.each_value {|hash|
+      instance_variable_set(hash[:ivar], hash[:get_state].call(state))
+    }
     each_child {|c| c.load_state(state.substate(c.component_name)) }
   end
 
@@ -14,6 +75,9 @@ class Component
   # +state+.
   #
   def dump_state(state)
+    self.class.state.each_value {|hash|
+      hash[:set_state].call(state, instance_variable_get(hash[:ivar]))
+    }
     each_child {|c| c.dump_state(state.substate(c.component_name))}
     state
   end 
